c07a74c18ad71294d88214d0d308e66e786395f3
tstuart <tstuart@beckshybrids.com>
2015-04-28 12:35:53 -0400

    Added {user} is typing functionality

--- server.js
+++ server.js
@@ -10,18 +10,30 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+
   socket.on('disconnect', function(){
     io.emit('chat message', clients[socket.id] + ' has disconnected');
     delete clients[socket.id];
   });
+
   socket.on('chat message', function(msg, nickName) {
     socket.broadcast.emit('chat message', nickName + ': ' + msg);
   });
+
   socket.on('User Connected', function(nickName) {
     // register Client
     clients[socket.id] = nickName;
     io.emit('chat message', nickName + ' has connected.');
   });
+
+  socket.on('keydown', function(nickName) {
+    socket.broadcast.emit('keydown', nickName);
+  });
+
+  socket.on('keyup', function(nickName) {
+    socket.broadcast.emit('keyup', nickName);
+  });
+
 });
 
 http.listen(3000, function(){
